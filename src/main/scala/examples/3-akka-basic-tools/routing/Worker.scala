67bdc1a1d0ad84da01463f410f45d10ec53d51bb
Cristina Delgado <cris3wdev@gmail.com>
2017-03-12 13:35:41 +0100

    Sending Messages via Router

---
+++ src/main/scala/examples/3-akka-basic-tools/routing/Worker.scala
@@ -0,0 +1,19 @@
+package examples
+
+import akka.actor.Actor
+
+
+class Worker extends Actor {
+  import Worker._
+
+  def receive = {
+    case msg: Work =>
+      println(s"I received Work Message and My ActorRef: ${self}")
+  }
+}
+
+
+object Worker {
+
+  case class Work()
+}
